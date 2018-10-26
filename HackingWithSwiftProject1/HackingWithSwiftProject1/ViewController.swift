bf55083562de34e323ed2168ad5c55e4c6be0de5
wccosby <cosby.william.c@gmail.com>
2018-10-26 19:44:42 +1100

    init Project1
    Starting project 1

---
+++ HackingWithSwiftProject1/HackingWithSwiftProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackingWithSwiftProject1
+//
+//  Created by William Cosby on 10/26/18.
+//  Copyright © 2018 William Cosby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
