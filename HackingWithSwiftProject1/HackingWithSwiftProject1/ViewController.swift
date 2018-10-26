6c95267b6ba9cb48eb1d080092583118be02a25b
wccosby <cosby.william.c@gmail.com>
2018-10-26 20:06:23 +1100

    Finished initial lesson
    Calling it for the day ;)

--- HackingWithSwiftProject1/HackingWithSwiftProject1/ViewController.swift
+++ HackingWithSwiftProject1/HackingWithSwiftProject1/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pictures = [String]() // the parantheses tell swift to create this right now.
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let fm = FileManager.default // data type that lets us work with the file system.
+        let path = Bundle.main.resourcePath! // set to the resource path of the bundle --> directory containing the program and its assets
+        let items = try! fm.contentsOfDirectory(atPath: path) // returns a list of the items in the contents of atPath's value
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+                print(pictures)
+            }
+        }
     }
 
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // dispose of any resources that can be recreated
+    }
 }
 
