f253e805650ec396e124f3b7f14b4965d9e31421
wccosby <cosby.william.c@gmail.com>
2018-10-28 14:45:16 +1100

    Beginning Project 2
    Saving project checkpoint

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by William Cosby on 10/28/18.
+//  Copyright © 2018 William Cosby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
