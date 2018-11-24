5b83ed499744bb45244f4b94013458b2f24b4a0f
wccosby <cosby.william.c@gmail.com>
2018-11-24 18:50:44 +1100

    Starting project 7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by William Cosby on 11/24/18.
+//  Copyright © 2018 William Cosby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
