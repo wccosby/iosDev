b92f7061d919ac43b45161539d107f1f13c6fb47
wccosby <cosby.william.c@gmail.com>
2018-12-04 13:49:34 +1100

    starting project10

---
+++ project10/project10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project10
+//
+//  Created by William Cosby on 12/4/18.
+//  Copyright © 2018 William Cosby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
