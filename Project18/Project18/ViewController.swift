a1d59367e2798a17137c06a4283b3a648162c2ee
wccosby <cosby.william.c@gmail.com>
2018-12-15 20:38:10 +1100

    starting project 19

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by William Cosby on 12/15/18.
+//  Copyright © 2018 William Cosby. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
